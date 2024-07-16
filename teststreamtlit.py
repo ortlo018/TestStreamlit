c9ad818b7e9094366d5054cbbfb22e714b76f85f
Cody Ortloff <145167820+ortlo018@users.noreply.github.com>
2024-07-15 20:19:38 -0500

    Update teststreamtlit.py

--- teststreamtlit.py
+++ teststreamtlit.py
@@ -6,5 +6,13 @@ Created on Mon Jul 15 17:50:45 2024
 """
 
 import streamlit as st
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+import random
+
+# Create an input box to enter a number
+input_number = st.number_input("Enter a number", min_value=0, value=10, step=1)
+
+# Generate a random number between the input number and 0
+random_number = random.uniform(0, input_number)
+
+# Display the result
+st.write("Random number between 0 and", input_number, "is", random_number)
